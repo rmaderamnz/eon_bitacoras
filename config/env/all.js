ca5561370cb48a5820a6ef83b875a10249c9667f
rmaderamnz <rmadera@iMac-de-Omar.local>
2015-11-23 18:23:56 -0600

    Angular material

--- config/env/all.js
+++ config/env/all.js
@@ -18,6 +18,8 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.js',
+				'public/lib/angular-material/angular-material.js',
+				'public/lib/angular-aria/angular-aria.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-cookies/angular-cookies.js', 
 				'public/lib/angular-animate/angular-animate.js', 
